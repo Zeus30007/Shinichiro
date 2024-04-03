be460b18ffc2ce3ddf42361d4aaeec16e299085d
Mello <130933523+Zeus30007@users.noreply.github.com>
2024-04-04 00:08:13 +0200

    𝙈𝙄𝙆𝙀𝙔㊗𝙎𝙝𝙞𝙣𝙞𝙘𝙝𝙞𝙧𝙤

--- settings.js
+++ settings.js
@@ -14,28 +14,28 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: mirai.art" //ur yt chanel name
+global.socialm = "GitHub: Zeus30007" //ur github or insta name
+global.location = "Australia, Sydney, newtown" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
+global.botname = '㊗𝙎𝙝𝙞𝙣𝙞𝙘𝙝𝙞𝙧𝙤' //ur bot name
+global.ownernumber = '61485971304' //ur owner number
+global.ownername = '㊗𝙈𝙄𝙆𝙀𝙔' //ur owner name
 global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.wagc = "https://chat.whatsapp.com/Ezy0NeJe5DQ0o1srrAtsUo"
+global.themeemoji = '⛩️'
+global.wm = "𝙈𝙄𝙆𝙀𝙔㊗𝙎𝙝𝙞𝙣𝙞𝙘𝙝𝙞𝙧𝙤."
+global.botscript = 'https://github.com/' //script link
+global.packname = "𝙎𝙝𝙞𝙣𝙞𝙘𝙝𝙞𝙧𝙤"
+global.author = "㊗𝙈𝙄𝙆𝙀𝙔"
+global.creator = "61485971304@s.whatsapp.net"
+global.xprefix = '!'
+global.premium = ["61485971304"] // Premium User
 global.hituet = 0
 
 //bot sett
-global.typemenu = 'v8' // menu type 'v1' => 'v8'
+global.typemenu = 'v1' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
